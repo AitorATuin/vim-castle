967b4e6c0b6c83dcdad9708982b1ffdc2627c291
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2015-01-11 22:10:12 +0100

    Added install script for php tools

--- home/.php/install-composer.sh
+++ home/.php/install-composer.sh
@@ -1,2 +1,25 @@
 # Script to install composer.phar
-curl -sS https://getcomposer.org/installer | php
+
+COMPOSER_PHAR=composer.phar
+VIM_PATH=$HOME/.vim 
+VIM_BIN_PATH=$VIM_PATH/bin
+PHP_PATH=$HOME/.php
+PHP_BIN_PATH=$PHP_PATH/vendor/bin
+
+echo "Checking composer.phar ... "
+[ -f $COMPOSER_PHAR ] || {
+    curl -sS https://getcomposer.org/installer | php
+}
+
+echo "Installing php tools ... "
+php composer.phar install && {
+    echo "Linking php tools ... "
+    [ -d ${VIM_BIN_PATH} ] || mkdir ${VIM_BIN_PATH}
+
+    ln -s ${PHP_BIN_PATH}/pdepend ${VIM_BIN_PATH}
+    ln -s ${PHP_BIN_PATH}/phpcfb ${VIM_BIN_PATH}
+    ln -s ${PHP_BIN_PATH}/phpcs ${VIM_BIN_PATH}
+    ln -s ${PHP_BIN_PATH}/phpmd ${VIM_BIN_PATH}
+} || {
+    echo "Error when installing php tools, aborting"
+}
